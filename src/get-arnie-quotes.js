ce46e457c8cbf4b0a4c7b32281b12108a39e3ea6
JanetZhuo <45689833+JanetZhuo@users.noreply.github.com>
2022-03-20 20:47:51 +1100

    feature: implement getArnieQuotes function

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,26 @@
-const { httpGet } = require('./mock-http-interface');
+const { httpGet } = require("./mock-http-interface");
+
+const successResponseKey = "Arnie Quote";
+const failResponseKey = "FAILURE";
+
+const getSingleArnieQuote = async (url) => {
+  try {
+    const response = await httpGet(url);
+    const { body, status } = response;
+    const message = JSON.parse(body).message;
+    const responseKey =
+      status === 200 ? [successResponseKey] : [failResponseKey];
+    return { [responseKey]: message };
+  } catch ({ message }) {
+    console.log(`Error: ${message}`);
+  }
+};
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const results = await Promise.all(
+    urls.map(async (url) => getSingleArnieQuote(url))
+  );
+  return results;
 };
 
 module.exports = {
